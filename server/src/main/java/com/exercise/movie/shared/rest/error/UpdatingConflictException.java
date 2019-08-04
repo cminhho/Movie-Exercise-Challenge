01ce38d9fbce11e753a5a06aad6a464c46be8d9b
Chung Ho <hmchung@onixdev2.com>
2019-08-04 20:42:38 +0700

    Handle REST API error response

--- server/src/main/java/com/exercise/movie/shared/rest/error/UpdatingConflictException.java
+++ server/src/main/java/com/exercise/movie/shared/rest/error/UpdatingConflictException.java
@@ -3,10 +3,10 @@ package com.exercise.movie.shared.rest.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BadRequestException extends RuntimeException {
+@ResponseStatus(HttpStatus.CONFLICT)
+public class UpdatingConflictException extends RuntimeException {
 
-  public BadRequestException(String message) {
+  public UpdatingConflictException(String message) {
     super(message);
   }
 }
